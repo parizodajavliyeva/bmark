6f5cdc27580c171b9c9a1a75dd0205f8cfbb21b9
odina <parizodajavliyeva08@gmail.com>
2024-01-18 18:47:28 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+elItem = document.querySelector(".site-header__item") ; 
+elList = document.querySelector(".sublist") ; 
+
+elItem.addEventListener ('click' , function(){
+    elList.classList.toggle('list-open') ; 
+})
+
+elItem1 = document.querySelector(".site-header__item-one") ; 
+elList1 = document.querySelector(".sublist-one") ; 
+
+elItem1.addEventListener ('click' , function(){
+    elList1.classList.toggle('list-open') ; 
+})
+
+elItem2 = document.querySelector(".site-header__item-two") ; 
+elList2 = document.querySelector(".sublist-two") ; 
+
+elItem2.addEventListener ('click' , function(){
+    elList2.classList.toggle('list-open') ; 
+})
+
+elItem3 = document.querySelector(".site-header__item-three") ; 
+elList3 = document.querySelector(".sublist-three") ; 
+
+elItem3.addEventListener ('click' , function(){
+    elList3.classList.toggle('list-open') ; 
+})
+
+
+elItem4 = document.querySelector(".site-header__item-four") ; 
+elList4 = document.querySelector(".sublist-four") ; 
+
+elItem4.addEventListener ('click' , function(){
+    elList4.classList.toggle('list-open') ; 
+})
+
+
+elItem5 = document.querySelector(".site-header__item-five") ; 
+elList5 = document.querySelector(".sublist-five") ; 
+
+elItem5.addEventListener ('click' , function(){
+    elList5.classList.toggle('list-open') ; 
+})
+
+
